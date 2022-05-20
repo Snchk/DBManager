9b2aceab957e8bc276e841643584841dec7842c0
Котул Олександр Юрійович <kotul@fuib.com>
2022-05-20 10:37:07 +0300

    fix

--- app.js
+++ app.js
@@ -35,10 +35,10 @@ app.use(`/api/auth`, authRoutes)
 app.use(`/api/client`, clientRoutes)
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/dist'))
+    app.use(express.static(__dirname +'client/dist/client'))
 
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'dist','client', 'index.html'))
+        res.sendFile(path.resolve(__dirname+ 'client', 'dist','client', 'index.html'))
     })
 }
 
